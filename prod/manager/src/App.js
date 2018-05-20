8543b5299c9bcdbc4d92361b0ef9f41618fb42c5
Unknown <chrisrey95@gmail.com>
2018-05-20 15:49:35 -0400

    Complete Section 14
    Boilerplate setup for manager app

---
+++ prod/manager/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+
+class App extends Component {
+
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyAEXv4zn1zaWbpmwPK7HlDK2AwaP_OiMBo',
+            authDomain: 'manager-2caf8.firebaseapp.com',
+            databaseURL: 'https://manager-2caf8.firebaseio.com',
+            projectId: 'manager-2caf8',
+            storageBucket: 'manager-2caf8.appspot.com',
+            messagingSenderId: '419666564688'
+          };
+          
+          firebase.initializeApp(config);
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
